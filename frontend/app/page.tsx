36b53b98fa909801495be24c7f09b4ae1f382c74
Muhammad Ahsan Ali Khan <alikhanahsan.11@gmail.com>
2026-01-31 16:01:14 +0500

    feat: initialize frontend with Next.js, TypeScript, and Tailwind CSS and improved project folder structure.
    - Added package.json for managing dependencies and scripts.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Configured TypeScript with tsconfig.json for strict type checking and module resolution.

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="p-4 bg-black text-white font-bold text-center">
+      Welcome to real time doc sharing application.
+    </div>
+  );
+}
