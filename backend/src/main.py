023a96049466d4dc1ceb6da3a64c00ee9a45c2b9
Anas-init <anastech900@gmail.com>
2026-02-10 18:15:54 +0500

    Routers registered

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from document.router import router as doc_router 
+from user.router import router as user_router
+app=FastAPI(title="Docs App")
 
-app=FastAPI()
+app.include_router(doc_router)
+app.include_router(user_router)
 
-@app.get('/')
-async def get_hello():
-    return {"Hello FAST API"}
+@app.get("/")
+def read_root():
+    return {"Router registered"}
+
